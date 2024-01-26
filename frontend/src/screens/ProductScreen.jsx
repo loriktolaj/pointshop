14f83224c730ae0a2cecf9da61d44b917133cdd6
Loriki <lt47136@ubt-uni.net>
2024-01-26 23:04:35 +0100

    redux creation

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+import Message from '../components/Message'
+
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState({});
-    const { id: productId } = useParams();
     
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [productId]);
+    const { id: productId } = useParams();
+
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+    
 
   return <>
     <Link className='btn btn-light my-3' to='/'>
         Go Back
     </Link>
-    <Row>
+    {isLoading? (
+        <Loader/>
+    ) : error ? (
+        <Message variant='danger'>{ error?.data?.message || error.error }</Message>
+    ) : (
+        <Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid/>
         </Col>
@@ -68,6 +71,7 @@ const ProductScreen = () => {
             </Card>
         </Col>
     </Row>
+    ) }
   </>
 }
 
